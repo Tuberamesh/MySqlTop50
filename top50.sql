cd0a4f2645e3c340385df8ab9422685131661338
Tuberamesh <r6904633@gmail.com>
2026-03-25 22:29:19 +0530

    4th  question

--- top50.sql
+++ top50.sql
@@ -16,9 +16,19 @@ from World
 where area >= 3000000 or population >=25000000
  
  
- 
+
  # 3rd question of top 50
  # Write a MySQL query to find the name of all customers who have not been referred by the customer with id 2.
 SELECT name
 FROM Customer
 WHERE referee_id != 2 OR referee_id IS NULL;
+
+
+# 4th question of top 50
+# Write a MySQL query to find the id of all authors who have viewed their own articles
+select distinct author_id as id
+from Views
+where author_id=viewer_id
+order by id asc
+#similar to above but with group by instead of distinct
+#select author_id as id from Views where author_id=viewer_id group by author_id order by author_id
