39334d4eab42070e6ccc5527a70c29da86a9bac3
Tuberamesh <r6904633@gmail.com>
2026-03-25 22:13:01 +0530

    2nd question

--- top50.sql
+++ top50.sql
@@ -8,5 +8,11 @@ and
 recyclable ='Y'
 
 
+# 2nd question of top 50
 
+# Write a MySQL query to find the name, population and area of all countries in the World database where the area is larger than 3 million or the population is larger than 25 million.
+select name, population, area
+from World
+where area >= 3000000 or population >=25000000
+ 
  
