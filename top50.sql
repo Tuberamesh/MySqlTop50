09be9e74af57cae48309db9c681f4ae189259a11
Tuberamesh <r6904633@gmail.com>
2026-04-01 18:51:11 +0530

    prices chenge date

--- top50.sql
+++ top50.sql
@@ -393,3 +393,23 @@ join Logs l2 on l1.id=l2.id-1
 join Logs l3 on l1.id=l3.id-2
 
 where l1.num =l2.num and l2.num =l3.num
+
+
+# 34th question of top 50
+# Write a MySQL query to find the product_id and price of all products in the Products
+
+SELECT product_id, new_price AS price
+FROM Products
+WHERE (product_id, change_date) IN (
+    SELECT product_id, MAX(change_date)
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+)
+
+UNION
+
+SELECT DISTINCT product_id, 10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16';
