bf2f4ccab4119153bea7d568363e52be4cbe72f8
Tuberamesh <r6904633@gmail.com>
2026-04-01 18:41:58 +0530

    consecutive

--- top50.sql
+++ top50.sql
@@ -382,3 +382,14 @@ select x, y, z,
 case when x+y>z and x+z>y and y+z>x then 'Yes' else 'No' end as triangle
 
 from Triangle
+
+
+# 33rd question of top 50
+# Write a MySQL query to find the id of all logs that have the same num as
+
+select distinct l2.num as ConsecutiveNums
+from Logs l1
+join Logs l2 on l1.id=l2.id-1
+join Logs l3 on l1.id=l3.id-2
+
+where l1.num =l2.num and l2.num =l3.num
