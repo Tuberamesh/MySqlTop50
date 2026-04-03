d7a8da75733aace6b0d74b0e14557cc802f35c09
Tuberamesh <r6904633@gmail.com>
2026-04-03 23:21:45 +0530

    top3 salary by dept

--- top50.sql
+++ top50.sql
@@ -549,3 +549,24 @@ AND (lat, lon) IN (
     GROUP BY lat, lon
     HAVING COUNT(*) = 1
 );
+
+
+
+
+# 43rd question of top 50
+# Write a MySQL query to find the department, employee and salary of all employees who are among the top 3 highest paid employees in their respective departments
+
+SELECT 
+    Department, 
+    Employee, 
+    Salary
+FROM (
+    SELECT 
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
+        DENSE_RANK() OVER (PARTITION BY e.departmentId ORDER BY e.salary DESC) AS rnk
+    FROM Employee e
+    JOIN Department d ON e.departmentId = d.id
+) AS ranked_salaries
+WHERE rnk <= 3;
