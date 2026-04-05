1a2ba044f7ae545a9b4547b50a52cf1791b3bca1
Tuberamesh <r6904633@gmail.com>
2026-04-05 18:20:20 +0530

    lissting the prodcuts have 100 units

--- top50.sql
+++ top50.sql
@@ -633,3 +633,18 @@ SELECT
 FROM Activities
 GROUP BY sell_date
 ORDER BY sell_date;
+
+
+
+
+# 49th question of top 50
+#lissting the prodcuts have 100 units
+
+SELECT 
+    p.product_name, 
+    SUM(o.unit) AS unit
+FROM Products p
+JOIN Orders o ON p.product_id = o.product_id
+WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY p.product_id
+HAVING unit >= 100;
