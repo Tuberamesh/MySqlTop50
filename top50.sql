ad60620dfeb3697dbf88eb21c691d02a0974b55e
Tuberamesh <r6904633@gmail.com>
2026-04-05 18:15:49 +0530

    group concat

--- top50.sql
+++ top50.sql
@@ -621,3 +621,15 @@ SELECT (
     ORDER BY salary DESC
     LIMIT 1 OFFSET 1
 ) AS SecondHighestSalary;
+
+
+
+# 48th question of top 50
+#group concate
+SELECT 
+    sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    GROUP_CONCAT(DISTINCT product ORDER BY product ASC SEPARATOR ',') AS products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date;
